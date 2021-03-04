317850fecf451f3e66b89aabaf0c6aaed4018de1
juhick <qhq1324@vip.qq.com>
2021-03-04 17:07:40 +0800

    斐波那契数

---
+++ DynamicPlanning/leetcode509.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n < 1) return 0;
+        if(n == 1 || n == 2) return 1;
+        int prev = 1, curr = 1;
+        for(int i = 3; i <= n; ++i){
+            int sum = curr + prev;
+            prev = curr;
+            curr = sum;
+        }
+        return curr;
+    }
+};
+
